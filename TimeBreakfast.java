c4f70373aa4daa9731856de19f6707b1408f495d
CinthiaOchoa <126133546+CinthiaOchoa@users.noreply.github.com>
2023-05-31 03:54:12 -0700

    Delete TimeBreakfast.java

--- TimeBreakfast.java
+++
@@ -1,9 +0,0 @@
-public class TimeBreakfast {
-    public static void main(String[] args) {
-     //   int first = 8 *60 +15:
-      //  int next 7*60 *3 +(12*3)
-              //  firstmile *2 + nesxtThreeMile
-                   //     int time minutes = total /60
-                         //       int time second = total % 60
-    }
-}
